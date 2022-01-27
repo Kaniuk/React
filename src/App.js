03822499b2212971f6d3e2c9a0717d6541dab41c
Liudmyla <lyudmula1989@ukr.net>
2022-01-27 21:46:03 +0200

    Add cats in classwork 4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useReducer} from 'react';
+import {useForm} from 'react-hook-form';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const petInitState = {
+    cats: [],
+};
+
+const petReducer = (state, action) => {
+    switch (action.type) {
+        case 'addCat':
+            return {...state, cats: [...state.cats, action.payload.name]};
+        default:
+            return state;
+    }
+};
+
+const App = () => {
+    const [petsState, dispatch] = useReducer(petReducer, petInitState);
+    const {register, handleSubmit, reset, formState: {errors}} = useForm();
+
+    const handleAddCat = (data) => {
+        dispatch({type: 'addCat', payload: {name: data.catName}});
+        reset()
+    };
+    return (
+        <div>
+            <form onSubmit={handleSubmit(handleAddCat)}>
+                <input {...register("catName")} placeholder="введи ім'я котіку"/>
+                <button>Add</button>
+            </form>
+            {petsState.cats.map(catName => <div>
+                {catName}
+            </div>)}
+        </div>
+    );
+};
 
 export default App;
