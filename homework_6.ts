8d43f10cd218cdf9885aafb4df884d405ab013dd
Liudmyla <lyudmula1989@ukr.net>
2022-02-03 16:10:35 +0200

    Add homework 6

---
+++ homework_6.ts
@@ -0,0 +1,69 @@
+import {IMission} from "./massion.interface";
+import {IUser} from "./homework_6_2";
+
+
+// 1) создать интерфейс на основе этого объекта:
+
+const mission: IMission = {
+    mission_name: 'Starlink-15 (v1.0)', launch_date_local: "2020-10-24T11:31:00-04:00",
+    launch_site:
+        {
+            site_name_long: "Cape Canaveral Air Force Station Space Launch Complex 40"
+        },
+    links: {
+        article_link: null,
+        video_link: "https://youtu.be/J442-ti-Dhg"
+    },
+    rocket: {
+        rocket_name: "Falcon 9",
+        first_stage: {
+            cores: [
+                {
+                    flight: 7,
+                    core: {
+                        reuse_count: 6,
+                        status: "unknown"
+                    }
+                }
+            ]
+        },
+        second_stage: {
+            payloads: [{
+                payload_type: 'Satellite',
+                payload_mass_kg: 15400,
+                payload_mass_lbs: 33951.2
+            }]
+        }
+    }
+}
+
+console.log(mission);
+
+const partialMission = (mission: Partial<IMission>): Partial<IMission> => {
+    return mission
+}
+
+partialMission({mission_name: 'Starlink-15 (v1.0)'})
+
+
+
+// 2) протипизировать функции:
+
+function sum(a: number, b: number): number {
+    return a + b;
+}
+
+function showSum(a: number, b: number): void {
+    console.log(a + b);
+}
+
+function incAge(someUser: IUser, inc: number): IUser {
+    someUser.age += inc
+    return someUser
+}
+console.log(sum(1, 2));
+showSum(2,3)
+incAge({name:'Valera',age:22,gender:'male'}, 2)
+
+
+
---
+++ massion.interface.ts
@@ -0,0 +1,39 @@
+export interface ICore {
+    flight: number;
+    core: {
+        reuse_count: number;
+        status: string;
+    }
+}
+
+/*
+export interface ILinks {
+    [linkKey: string]: string | null
+}*/
+
+interface IPayload {
+    payload_type: string;
+    payload_mass_kg: number;
+    payload_mass_lbs: number
+}
+
+export interface IMission {
+    mission_name: string;
+    launch_date_local: string;
+    launch_site: {
+        site_name_long: string;
+    }
+    links: {
+        article_link: null | string;
+        video_link: string
+    }
+    rocket: {
+        rocket_name: string;
+        first_stage: {
+            cores: ICore[];
+        }
+        second_stage: {
+            payloads: IPayload[]
+        }
+    }
+}
